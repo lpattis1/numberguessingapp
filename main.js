c4dac36e8a2850f77f103d76fb457f2754974ec8
Lauren <lpattis1@asu.edu>
2021-03-30 20:03:18 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+let btnGuess = document.querySelector(".btns__color--guess");
+let btnReset = document.querySelector(".btns__color--reset");
+
+function randomNum(){
+    // variables:
+    let numGuess = document.querySelector(".color-input__guess").value;
+    let showGuess = document.querySelector(".answers__show-guess").innerHTML = numGuess;
+    
+    let numAnswer = Math.floor(Math.random() * 10);
+    console.log(numAnswer);
+
+    if(numGuess > numAnswer || numGuess < numAnswer) {
+        document.querySelector(".answers__show-guess").style.fontSize = "25rem";
+        document.querySelector(".win-lose__try-again").style.opacity = 1;
+        document.querySelector(".win-lose__win").style.opacity = 0;
+        document.querySelector(".color-box").style.background = "red";
+        document.querySelector(".color-input__guess").style.color = "red";
+    } else {
+        document.querySelector(".answers__show-guess").style.fontSize = "25rem";
+        document.querySelector(".win-lose__win").style.opacity = 1;
+        document.querySelector(".color-box").style.background = "green";
+        document.querySelector(".color-input__guess").style.color = "green";
+        document.querySelector(".win-lose__try-again").style.opacity = 0;
+    }
+}
+
+btnGuess.addEventListener("click", function(){
+    randomNum();
+})
